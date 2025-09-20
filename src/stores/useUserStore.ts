4a3704ca7b87f22dc9eccf26a8aad925b7e622b3
Helton Brito <heltoncarlospa@hotmail.com>
2025-09-20 13:38:44 -0300

    feat: add user store

---
+++ src/stores/useUserStore.ts
@@ -0,0 +1,23 @@
+import { defineStore } from 'pinia';
+import type { User } from '../types/user';
+import { ref, computed } from 'vue';
+
+export const useUserStore = defineStore('user', () => {
+  const user = ref<User | null>(null);
+  const isAuthenticated = computed(() => !!user.value);
+
+  function setUser(newUser: User) {
+    user.value = newUser;
+  }
+
+  function clearUser() {
+    user.value = null;
+  }
+
+  return {
+    user,
+    isAuthenticated,
+    setUser,
+    clearUser,
+  };
+});
