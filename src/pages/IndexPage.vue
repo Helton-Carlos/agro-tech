f70f3bb53e4b8660153b890a9bc77fd8baf3fcb6
Helton Brito <heltoncarlospa@hotmail.com>
2025-09-21 11:14:02 -0300

    feat: add card info  bank

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -25,6 +25,32 @@
         />
       </q-card>
     </div>
+
+    <q-card class="q-ma-md">
+      <q-card class="my-card">
+        <q-card-section class="bg-teal text-white">
+          <div class="text-h6">Créditos Agro</div>
+        </q-card-section>
+
+        <q-card-section>
+          <p>
+            Os créditos agropecuários são linhas de financiamento voltadas para produtores rurais,
+            auxiliando na aquisição de insumos, máquinas e infraestrutura. Eles podem ser oferecidos
+            por diversos bancos com condições especiais para o setor agrícola.
+          </p>
+          <p>
+            Para saber mais sobre opções de crédito, consulte o
+            <a href="https://www.banconordeste.gov.br/" target="_blank" class="text-teal">
+              Banco do Nordeste </a
+            >.
+          </p>
+        </q-card-section>
+
+        <q-card-actions align="left">
+          <q-btn label="Saiba Mais" color="teal" flat unelevated @click="openBancoNordeste" />
+        </q-card-actions>
+      </q-card>
+    </q-card>
   </q-page>
 </template>
 
@@ -35,4 +61,8 @@ import { isTablet } from 'src/composable/useBreakpoints';
 import { computed } from 'vue';
 
 const widthCard = computed(() => (isTablet.value ? '350' : '750'));
+
+function openBancoNordeste() {
+  window.open('https://www.bnb.gov.br/', '_blank');
+}
 </script>
